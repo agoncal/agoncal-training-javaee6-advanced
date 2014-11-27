da798756208058655205b4629394ac15242e2f40
Antonio Goncalves <antonio.goncalves@gmail.com>
2014-11-27 21:24:55 +0100

    Fix missing Person

---
+++ after/08-WebApplication/src/main/java/org/agoncal/training/javaee6adv/model/Person.java
@@ -0,0 +1,89 @@
+package org.agoncal.training.javaee6adv.model;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@MappedSuperclass
+public class Person
+{
+
+   @Column(length = 50, name = "first_name")
+   @NotNull
+   @Size(min = 2, max = 50)
+   protected String firstName;
+
+   @Column(length = 50, name = "last_name")
+   @NotNull
+   @Size(min = 2, max = 50)
+   protected String lastName;
+
+   @Column(name = "date_of_birth")
+   @Temporal(TemporalType.DATE)
+   @Past
+   protected Date dateOfBirth;
+
+   @Transient
+   protected Integer age;
+
+   public String getFirstName()
+   {
+      return firstName;
+   }
+
+   public void setFirstName(String firstName)
+   {
+      this.firstName = firstName;
+   }
+
+   public String getLastName()
+   {
+      return lastName;
+   }
+
+   public void setLastName(String lastName)
+   {
+      this.lastName = lastName;
+   }
+
+   public Date getDateOfBirth()
+   {
+      return dateOfBirth;
+   }
+
+   public void setDateOfBirth(Date dateOfBirth)
+   {
+      this.dateOfBirth = dateOfBirth;
+   }
+
+   public Integer getAge()
+   {
+      return age;
+   }
+
+   public void setAge(Integer age)
+   {
+      this.age = age;
+   }
+
+   @Override
+   public String toString()
+   {
+      String result = getClass().getSimpleName() + " ";
+      if (firstName != null && !firstName.trim().isEmpty())
+         result += "firstName: " + firstName;
+      if (lastName != null && !lastName.trim().isEmpty())
+         result += ", lastName: " + lastName;
+      if (dateOfBirth != null)
+         result += ", dateOfBirth: " + dateOfBirth;
+      if (age != null)
+         result += ", age: " + age;
+      return result;
+   }
+}
